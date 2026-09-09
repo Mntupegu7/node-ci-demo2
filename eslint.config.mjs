4195803c58493459edd4e612b246aff400427f20
Mntupegu7 <oficialmintu2@gmail.com>
2026-09-09 23:28:22 +0530

    Added Github Actions CI

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import jest from "eslint-plugin-jest";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+
+  {
+    files: ["**/*.test.js"],
+    plugins: {
+      jest
+    },
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...jest.environments.globals.globals
+      }
+    }
+  }
+]);
